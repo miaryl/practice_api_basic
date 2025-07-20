57f0b7872df0438fc89fd85c58e8dde3206b1305
Mio <gusw.0246.lpsyg@gmail.com>
2025-07-20 23:36:00 +0200

    it works!

--- unselessjoke.js
+++ unselessjoke.js
@@ -1,1 +1,16 @@
 const btn = document.getElementById("fetchBtn"); 
+const showFact = document.getElementById("fact");
+
+btn.addEventListener('click', fetchFact);
+
+function fetchFact(){
+    fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then(response => response.json())
+    .then(data => {
+        showFact.textContent = data.text;
+    })
+    .catch(error => {
+        showFact.textContent = "sorry there is some error...";
+        console.error('error', error);
+    })
+}
